a58ae6bdc08c2fbf7542183b8aea2f1135c81628
frank <xiaosaihong2008@gmail.com>
2019-06-24 17:30:09 -0400

    format bridge-react

--- Code Examples/week5/bridge-react/src/components/BridgeInfo.js
+++ Code Examples/week5/bridge-react/src/components/BridgeInfo.js
@@ -1,36 +1,37 @@
-import React from 'react';
-import LeafletMap from './LeafletMap.js';
-import './BridgeInfo.css';
+import React from "react";
+import LeafletMap from "./LeafletMap.js";
+import "./BridgeInfo.css";
 
-function InfoPanel(props){
-    const bridge = props.bridge;
-    const age = (new Date()).getFullYear() - bridge.year;
+function InfoPanel(props) {
+  const bridge = props.bridge;
+  const age = new Date().getFullYear() - bridge.year;
 
-    return (
-        <div id="info-panel">
-            <h2>{bridge.nameEncoded}</h2>
-            <div className="bridge-stats">
-                <div>Year: {bridge.year} ({age} years)</div>
-                <div>Width: {bridge.width}m</div>
-                <div>Length: {bridge.length}m</div>
-            </div>
+  return (
+    <div id="info-panel">
+      <h2>{bridge.nameEncoded}</h2>
+      <div className="bridge-stats">
+        <div>
+          Year: {bridge.year} ({age} years)
         </div>
-    );
-    
+        <div>Width: {bridge.width}m</div>
+        <div>Length: {bridge.length}m</div>
+      </div>
+    </div>
+  );
 }
 
-function BridgeInfo(props){
-    const infoPanel = props.bridge ? <InfoPanel bridge = {props.bridge}/>: null;
+function BridgeInfo(props) {
+  const infoPanel = props.bridge ? <InfoPanel bridge={props.bridge} /> : null;
 
-    return (
-        <div id="bridge-info-wrapper">
-            {infoPanel}
+  return (
+    <div id="bridge-info-wrapper">
+      {infoPanel}
 
-            <div id="leaflet-map">
-                <LeafletMap bridge={props.bridge}/>
-            </div>
-        </div>
-    );
+      <div id="leaflet-map">
+        <LeafletMap bridge={props.bridge} />
+      </div>
+    </div>
+  );
 }
 
 export default BridgeInfo;
